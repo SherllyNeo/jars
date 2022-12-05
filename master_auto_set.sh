db9505f6ecc32c2b2598693cddc359a45c85f76b
User <jacobp.willo@gmail.com>
2022-12-05 01:23:20 +0000

    update: breaking out into multi scripts

--- master_auto_set.sh
+++ master_auto_set.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-sh ./dependency_installer
+sh ./dependency_installer.sh
 cd ~
-sh -c 'mkdir -p ~/bin'
-sh -c 'mkdir -p ~/backgrounds'
-
+mkdir -p ~/bin
+mkdir -p ~/backgrounds
 wget -c https://wallpaperaccess.com/full/1872.jpg -O ~/backgrounds/japanese_wallpaper.jpg
+
+sh ./git_cloner.sh
+sh ./make_suckless.sh
+sh ./fill_bin.sh
+sh ./set_up_xinit.sh
+
 echo "now getting git stuff..."
 
 
