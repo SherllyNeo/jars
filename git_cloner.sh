76f1ad24432e87143df41549e5bb1c13a4d73c3b
User <jacobp.willo@gmail.com>
2022-12-05 01:20:44 +0000

    update: breaking out into multi scripts

---
+++ git_cloner.sh
@@ -0,0 +1,6 @@
+mkdir -p ~/git
+
+git clone https://github.com/LukeSmithxyz/st.git ~/git/st
+git clone https://github.com/LukeSmithxyz/dwm.git ~/git/dwm
+git clone https://github.com/LukeSmithxyz/dwmblocks.git ~/git/dwmblocks
+git clone https://github.com/LukeSmithxyz/dmenu.git ~/git/dmenu
