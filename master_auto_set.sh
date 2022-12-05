60746da4c9fb88d8312aeb82fe51681f45028268
User <jacobp.willo@gmail.com>
2022-12-05 01:34:34 +0000

    update: breaking out into multi scripts

--- master_auto_set.sh
+++ master_auto_set.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-sh ./dependency_installer.sh
+# Full path of the current script
+THIS=`readlink -f "${BASH_SOURCE[0]}" 2>/dev/null||echo $0`
+
+# The directory where current script resides
+DIR=`dirname "${THIS}"`
+. "$DIR/dependency_installer.sh"
 cd ~
 mkdir -p ~/bin
 mkdir -p ~/backgrounds
 wget -c https://wallpaperaccess.com/full/1872.jpg -O ~/backgrounds/japanese_wallpaper.jpg
 
-sh ./git_cloner.sh
-sh ./make_suckless.sh
-sh ./fill_bin.sh
-sh ./set_up_xinit.sh
+. "$DIR/git_cloner.sh"
+. "$DIR/make_suckless.sh"
+. "$DIR/fill_bin.sh"
+. "$DIR/set_up_xinit.sh"
 
 echo "finished!"
 
